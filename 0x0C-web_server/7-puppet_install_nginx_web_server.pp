ff3c74dbb6f1004aa0e394b8e2451bc9f12c5fad
Mofyally02 <mofyally02@gmail.com>
2023-06-28 00:05:27 +0300

    Update Web_server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,40 @@
+# Installs and configures Nginx server.
+
+include stdlib
+
+exec { 'update':
+  command => 'apt-get -yqq update',
+  path    => '/usr/bin/:/usr/sbin/:/bin'
+}
+
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['update'],
+}
+
+$string = '
+        # 301 redirect for /redirect_me
+        rewrite ^/redirect_me/?$ https://youtube.com/v=QH2-TGUlwu4 permanent;
+}'
+
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => inline_template('<%= "Hello World!\n " %>'),
+  notify  => Service['nginx'],
+  require => Package['nginx']
+}
+
+file_line { '301 redirect when quering /redirect_me':
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  line    => $string,
+  match   => '^}$',
+  replace => true,
+  notify  => Service['nginx'],
+  require => Package['nginx']
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
