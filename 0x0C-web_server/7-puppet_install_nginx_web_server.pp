9036cef6400f02d05b5415aafe8bafd325fe31df
Mofyally02 <mofyally02@gmail.com>
2023-06-28 08:14:21 +0300

    Update Web_server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,39 +1,78 @@
-# Installs and configures Nginx server.
-
-include stdlib
-
-exec { 'update':
-  command => 'apt-get -yqq update',
-  path    => '/usr/bin/:/usr/sbin/:/bin'
+# adds stable version of nginx
+exec { 'add nginx stable repo':
+  command => 'sudo add-apt-repository ppa:nginx/stable',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
+# updating software packages list
+exec { 'update packages':
+  command => 'apt-get update',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+# installing nginx
 package { 'nginx':
-  ensure  => installed,
-  require => Exec['update'],
+  ensure     => 'installed',
 }
 
-$string = '
-        # 301 redirect for /redirect_me
-        rewrite ^/redirect_me/?$ https://youtube.com/v=QH2-TGUlwu4 permanent;
-}'
+# allows HTTP
+exec { 'allow HTTP':
+  command => "ufw allow 'Nginx HTTP'",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif  => '! dpkg -l nginx | egrep \'îi.*nginx\' > /dev/null 2>&1',
+}
 
+# changes folder rights
+exec { 'chmod www folder':
+  command => 'chmod -R 755 /var/www',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+# creating an index file
 file { '/var/www/html/index.html':
-  ensure  => present,
-  content => inline_template('<%= "Hello World!\n " %>'),
-  notify  => Service['nginx'],
-  require => Package['nginx']
+  content => "Hello World!\n",
 }
 
-file_line { '301 redirect when quering /redirect_me':
-  ensure  => present,
-  path    => '/etc/nginx/sites-available/default',
-  line    => $string,
-  match   => '^}$',
-  replace => true,
-  notify  => Service['nginx'],
-  require => Package['nginx']
+# creating an index file
+file { '/var/www/html/404.html':
+  content => "Ceci n'est pas une page\n",
 }
 
+# adds redirection and error page
+file { 'Nginx default config file':
+  ensure  => file,
+  path    => '/etc/nginx/sites-enabled/default',
+  content =>
+"server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+               root /var/www/html;
+        # Add index.php to the list if you are using PHP
+        index index.html index.htm index.nginx-debian.html;
+        server_name _;
+        location / {
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+                try_files \$uri \$uri/ =404;
+        }
+        error_page 404 /404.html;
+        location  /404.html {
+            internal;
+        }
+
+        if (\$request_filename ~ redirect_me){
+            rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+        }
+}
+",
+}
+# restarts nginx
+exec { 'restart service':
+  command => 'service nginx restart',
+  path    => '/usr/bin:/usr/sbin:/bin',
+}
+
+# starts service nginx
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
